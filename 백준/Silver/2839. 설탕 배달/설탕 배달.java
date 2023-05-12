c653196cdc3ec2930b7cdc35f68c09fdd1bf6f0b
oh5chaeyoung <110815151+oh5chaeyoung@users.noreply.github.com>
2023-05-12 16:52:28 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
@@ -9,25 +8,24 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 
-		int[] dp = new int[N+1];
-		
-		Arrays.fill(dp, -1);
-		dp[3] = 1;
-		if(N >= 5)
-			dp[5] = 1;
-		for(int i=3; i<=N; i++) {
-			if(dp[i-3] > 0)
-				dp[i] = dp[i-3] +1;
-			if(i > 5)
-				if(dp[i-5] > 0)
-					if(dp[i] > 0)
-						dp[i] = Math.min(dp[i], dp[i-5]+1);
-					else
-						dp[i] = dp[i-5] + 1;
+		int cnt = 0;
+		while(true) {
+			if(N % 5 == 0) {
+				cnt += N/5;
+				break;
+			}
+			else {
+				N -= 3;
+				cnt++;
+			}
+			
+			if(N < 0) {
+				cnt = -1;
+				break;
+			}
 		}
 		
-		System.out.println(dp[N]);
-		
+		System.out.println(cnt);	
 	}
 
 }
