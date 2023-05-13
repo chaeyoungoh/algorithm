c057570dbbc4935bba6ef58c7049a7624531867a
oh5chaeyoung <110815151+oh5chaeyoung@users.noreply.github.com>
2023-05-13 10:56:08 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 14672 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -6,75 +6,78 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static class Point {
-		int x, y;
-
-		public Point(int x, int y) {
-			super();
-			this.x = x;
-			this.y = y;
-		}
-	}
-	
-	static int N, M, ans;
-	static int[][] map;
-	static boolean[][] visited;
 
 	public static void main(String[] args) throws IOException {
-		StringBuilder sb = new StringBuilder();
+//		1. input
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		map = new int[N+1][M+1];
-		visited = new boolean[N+1][M+1];
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		
-		String s;
-		for(int i = 1; i <= N; i++) {
-			s = br.readLine();
-			for(int j = 1; j <= M; j++) {
-				map[i][j] = s.charAt(j - 1) - '0';
+		int[][] map = new int[N][M];
+		for(int i=0; i<N; i++) {
+			String s = br.readLine();
+			for(int j=0; j<M; j++) {
+				map[i][j] = s.charAt(j) - '0';
 			}
 		}
+				
+//		2. bfs
+//		3. output
+		System.out.println(bfs(map, N, M));
 		
-		bfs();
 		
-		System.out.println(ans);
 		
 	}
 
-	private static void bfs() {
+	static class Point {
+		int x, y, cnt;
+		public Point(int x, int y, int cnt) {
+			this.x = x;
+			this.y = y;
+			this.cnt = cnt;
+		}
+	}
+	
+	private static int bfs(int[][] map, int N, int M) {
+		int[] dx = {1, -1, 0, 0};
+		int[] dy = {0, 0, 1, -1};
+		boolean[][] ch = new boolean[N][M];
 		Queue<Point> que = new LinkedList<>();
-		que.offer(new Point(1, 1));
-		visited[1][1] = true;
+		que.offer(new Point(0, 0, 1));
+		ch[0][0] = true;
 		
-		int[] dx = {-1, 1, 0, 0};
-		int[] dy = {0, 0, -1, 1};
-		
-		int lv = 0;
 		while(!que.isEmpty()) {
-			lv++;
-			int len = que.size();
-			for(int i = 0; i < len; i++) {
-				Point tmp = que.poll();
-				if(tmp.x == N && tmp.y == M) {
-					ans = lv;
-					return;
-				}
-				for(int d = 0; d < dx.length; d++) {
-					int nx = tmp.x + dx[d];
-					int ny = tmp.y + dy[d];
-					if(outOfRange(nx, ny) || map[nx][ny] == 0 || visited[nx][ny]) continue;
-					visited[nx][ny] = true;
-					que.offer(new Point(nx, ny));
-				}
+			Point tmp = que.poll();
+			int x = tmp.x;
+			int y = tmp.y;
+			int cnt = tmp.cnt;
+			if(isGoal(x, y, N, M))	return cnt;
+
+			for(int d=0; d<dx.length; d++) {
+				int nx = x + dx[d];
+				int ny = y + dy[d];
+				if(outOfRange(nx, ny, N, M))	continue;
+				if(ch[nx][ny])	continue;
+				if(map[nx][ny] == 0)	continue;
+				que.offer(new Point(nx, ny, cnt+1));
+				ch[nx][ny] = true;				
 			}
+			
 		}
+		
+		return -1;
+		
 	}
 
-	private static boolean outOfRange(int nx, int ny) {
-		if(nx < 1 || nx > N || ny < 1 || ny > M)
+	private static boolean isGoal(int nx, int ny, int N, int M) {
+		if(nx == N-1 && ny == M-1)
+			return true;
+		return false;
+	}
+
+	private static boolean outOfRange(int nx, int ny, int N, int M) {
+		if(nx < 0 || nx >= N || ny < 0 || ny >= M)
 			return true;
 		return false;
 	}
