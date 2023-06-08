fb76ee338f64b102827d3d351e290354b1189fb3
oh5chaeyoung <110815151+oh5chaeyoung@users.noreply.github.com>
2023-06-08 17:12:59 +0900

    [Silver III] Title: 소수 구하기, Time: 236 ms, Memory: 20108 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int S, E;
+	static boolean[] justNums;
+	
+	public static void main(String[] args) throws IOException {
+		StringBuilder result = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		S = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		justNums = new boolean[E+1];
+		isPrime();
+		for(int i=S; i<=E; i++) {
+			if(justNums[i])	
+				continue;
+			else	
+				result.append(i).append('\n');
+		}
+		
+		System.out.println(result.toString());
+	}
+
+	private static void isPrime() {
+		justNums[0] = justNums[1] = true;
+		
+		for(int i=2; i<=E; i++) {
+			if(justNums[i])	continue;
+			int ni = i * 2;
+			while(ni <= E) {
+				justNums[ni] = true;
+				ni += i;
+			}
+		}		
+	}
+
+}
