33ad82dae358912e70bddc5c43a34b8c111c21b7
oh5chaeyoung <110815151+oh5chaeyoung@users.noreply.github.com>
2023-06-12 11:41:39 +0900

    [Silver IV] Title: 보물, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] A = new int[N];
+		int[] B = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++)
+			A[i] = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++)
+			B[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(A);
+		Arrays.sort(B);
+		
+		int result = 0;
+		for(int i=0; i<N; i++) {
+			result += (A[N-i-1] * B[i]);
+		}
+		System.out.println(result);
+	}
+	
+
+}
