25d95157b0f41fda5d0cb7faf9c1476a9951326a
oh5chaeyoung <110815151+oh5chaeyoung@users.noreply.github.com>
2024-06-06 16:18:30 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 14060 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -3,31 +3,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
-	static StringBuilder result = new StringBuilder();
-	static int cnt;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		for(int tc=0; tc<T; tc++) {
-			int N = Integer.parseInt(br.readLine());
-			cnt = 0;
-			backTracking(0, N);
-			result.append(cnt).append('\n');
-		}
-		
-		System.out.println(result.toString());
-	}
-
-	private static void backTracking(int sum, int n) {
-		if(sum >= n) {
-			if(sum == n)	cnt++;
-			return;
-		}
-		
-		backTracking(sum + 1, n);
-		backTracking(sum + 2, n);
-		backTracking(sum + 3, n);
-	}
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        int[] dp = solution();
+        for(int tc=0; tc<T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            sb.append(dp[N]).append('\n');
+        }
+        System.out.println(sb.toString());
+    }
+    private static int[] solution() {
+        int[] dp = new int[11];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for(int i=4; i<11; i++)
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        return dp;
+    }
 }
